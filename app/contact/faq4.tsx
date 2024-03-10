bdce7ff8f878294a62a4f20bb6b7eda4565f0be5
andre1melo1 <[andre1melo1@proton.me](mailto:andre1melo1@proton.me)>
2024-03-10 23:31:20 +0000

    &apos; fixes

--- app/contact/faq4.tsx
+++ app/contact/faq4.tsx
@@ -5,7 +5,7 @@ export default function FAQ4() {
   return (
     <Card className='h-full'>
         <CardFAQ>
-          Simply look for our name in the App Store or in the Android Market. In the Android Market, if you can't find our App, that likely means your phone is not supported - see the list of supported operating systems.
+          Simply look for our name in the App Store or in the Android Market. In the Android Market, if you can&apos;t find our App, that likely means your phone is not supported - see the list of supported operating systems.
         </CardFAQ>
     </Card>
   )
--- app/contact/faq5.tsx
+++ app/contact/faq5.tsx
@@ -5,7 +5,7 @@ export default function FAQ5() {
   return (
     <Card className='h-full'>
         <CardFAQ>
-          You do not have to log in to view addresses or maps to our office locations or even to get our contact information. Simply open the App and use the "Locations" link at the bottom of the login screen.
+          You do not have to log in to view addresses or maps to our office locations or even to get our contact information. Simply open the App and use the link at the bottom of the login screen.
         </CardFAQ>
     </Card>
   )
